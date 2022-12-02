0eafbd1130c41ed0ec9bbb2bc3b30a13d7da43a1
Louis AUTHIE <65625876+LouisAUTHIE@users.noreply.github.com>
2022-12-02 22:40:11 +0100

    Add Chapter 8 Reinsurance module

---
+++ Chapter 8/whreinsurance/views/templates/hook/whreinsurance.tpl
@@ -0,0 +1,17 @@
+<div id="whreinsurance_container">
+  <div>
+    <img src="{$module_img_dir}wh1.png"/>
+    <span>Free delivery</span>
+    <p>Get free delivery<br/>for orders above 50&euro;</p>
+  </div>
+  <div>
+    <img src="{$module_img_dir}wh2.png"/>
+    <span>Secured payments</span>
+    <p>Every transaction is secured<br/>by our partner</p>
+  </div>
+  <div>
+    <img src="{$module_img_dir}wh3.png"/>
+    <span>Free returns</span>
+    <p>Don't hesitate, free return<br/>for your products within 15 days</p>
+  </div>
+</div>
---
+++ Chapter 8/whreinsurance/whreinsurance.php
@@ -0,0 +1,63 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class WHReinsurance extends Module
+{
+    public function __construct()
+    {
+        //Required attributes
+        $this->name = 'whreinsurance';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Web Helpers';
+
+        //Optional non-localized attributes
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = [
+            'min' => '1.7.0',
+            'max' => '8.99.99',
+        ];
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        //Optional localizable attributes
+        $this->bootstrap = true;
+        $this->confirmUninstall = $this->l('Do you still you want to uninstall this module?');
+        $this->description = $this->l('This is a reinsurance module for your front office.');
+        $this->displayName = $this->l('Reinsurance block');
+    }
+
+    public function install()
+    {
+        return parent::install() && $this->registerHook('displayFooterBefore') && $this->registerHook('actionFrontControllerSetMedia');
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall() && $this->unregisterHook('displayFooterBefore') && $this->unregisterHook('actionFrontControllerSetMedia');
+    }
+
+    public function hookDisplayFooterBefore($params)
+    {
+        $this->context->smarty->assign([
+            'module_img_dir' => $this->_path . 'views/img/',
+        ]);
+        return $this->display(__FILE__, 'whreinsurance.tpl');
+    }
+
+    public function hookActionFrontControllerSetMedia()
+    {
+        $this->context->controller->registerStylesheet(
+            'whreinsurance-style',
+            $this->_path.'views/css/whreinsurance.css',
+            [
+                'media' => 'all',
+                'priority' => 1000,
+            ]
+        );
+    }
+}
