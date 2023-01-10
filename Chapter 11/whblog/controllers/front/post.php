01031fc2c165f4b6a58c4a39e4c7c7ca5b5fc6a0
Louis AUTHIE <65625876+LouisAUTHIE@users.noreply.github.com>
2023-01-10 22:21:08 +0100

    Update post.php

--- Chapter 11/whblog/controllers/front/post.php
+++ Chapter 11/whblog/controllers/front/post.php
@@ -5,6 +5,23 @@ class WHBlogPostModuleFrontController extends ModuleFrontController
     public function initContent()
     {
         parent::initContent();
+
+        $categoryPostRepository = $this->get('webhelpers.whblog.repository.whblogcategorypost_repository');
+        $relationships = $categoryPostRepository->findBy(['post'=>Tools::getValue('id_post')]);
+
+        $categories = [];
+        if(!is_null($relationships) && count($relationships)>0)
+        {
+            foreach($relationships as $relation)
+            {
+                $id_category = $relation->getCategory()->getId();
+                $categoryRepository = $this->get('webhelpers.whblog.repository.whblogcategory_repository');
+                $category = $categoryRepository->findOneById($id_category);
+                $category_data = ["title"=>$category->getCategoryLangByLangId($this->context->language->id)->getTitle(),"url"=>Context::getContext()->link->getModuleLink('whblog', 'category', array('id_category' => $id_category))];
+                $categories[] = $category_data;
+            }
+        }
+
         $postRepository = $this->get('webhelpers.whblog.repository.whblogpost_repository');
         $post = $postRepository->findOneById(Tools::getValue('id_post'));
         $this->context->smarty->assign(
@@ -12,6 +29,7 @@ class WHBlogPostModuleFrontController extends ModuleFrontController
           'title' => $post->getTitleLangs(),
           'content' => $post->getContentLangs(),
           'filename' => $post->getFilename(),
+          'categories' => $categories,
         ));
         $this->setTemplate('module:whblog/views/templates/front/post.tpl');
     }
@@ -44,3 +62,4 @@ class WHBlogPostModuleFrontController extends ModuleFrontController
         return $breadcrumb;
      }
 }
+
